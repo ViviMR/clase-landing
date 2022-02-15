1ebdef93e9199a6826e622b4fcf1a135e602fb98
ViviMR <psrangel.6@gmail.com>
2022-02-15 11:09:21 -0500

    cambios generales
    Cambios en el HTML, anadi nuevos botones y los id correspondientes. En el CSS anadi estilos a nuevos botones. Finalmente, en en js agregue nuevas funcionalidades a los botones de 0 a 9.

--- javacript/calculadora/scripts.js
+++ javacript/calculadora/scripts.js
@@ -3,6 +3,14 @@ window.addEventListener("load", function () {
 
     let buttonOne = document.querySelector('#one')
     let buttonTwo = document.querySelector('#two')
+    let buttonThree = document.querySelector('#three')
+    let buttonFour = document.querySelector('#four')
+    let buttonFive = document.querySelector('#five')
+    let buttonSix = document.querySelector('#six')
+    let buttonSeven = document.querySelector('#seven')
+    let buttonEight = document.querySelector('#eight')
+    let buttonNine = document.querySelector('#nine')
+    let buttonZero = document.querySelector('#zero')
     let espacioDeLosNumeros = document.querySelector('#campo')
 
     buttonOne.addEventListener('click',function(){
@@ -14,5 +22,46 @@ window.addEventListener("load", function () {
         let valorDeLosNumeros = document.querySelector('#campo').value
         /* console.log('2') */
         espacioDeLosNumeros.value = valorDeLosNumeros+2
+    })
+    buttonThree.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+3
+    })
+    buttonFour.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+4
+    })
+    
+    buttonFive.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+5
+    })
+    buttonSix.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+6
+    })
+    buttonSeven.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+7
+    })
+    buttonEight.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+8
+    })
+    buttonNine.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+9
+    })
+    buttonZero.addEventListener('click',function(){
+        let valorDeLosNumeros = document.querySelector('#campo').value
+        /* console.log('2') */
+        espacioDeLosNumeros.value = valorDeLosNumeros+0
     })
 })
