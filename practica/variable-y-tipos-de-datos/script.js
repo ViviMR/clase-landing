50daba6086fa4995ecab695798ad9caa7e409eb6
ViviMR <psrangel.6@gmail.com>
2022-03-01 21:16:55 -0500

    Clase de tipos de variables y operadores

---
+++ practica/variable-y-tipos-de-datos/script.js
@@ -0,0 +1,97 @@
+console.log("Hola mi vidita, te amo mucho")
+
+let nombre = "Viviana";
+const apellido = "Moreno";
+console.log (nombre)
+console.log (apellido)
+
+nombre = "Andres";
+console.log(nombre)
+console.log (apellido)
+
+/*let nombreEjemplo = "Viviana";
+let edad = 29;
+let mayorEdad = true;
+let estatura = 1.57;
+
+console.log(typeof mayorEdad)*/
+
+/*Operadores*/
+
+/*
++
+
+-
+
+*
+
+/
+*/
+
+/*Operadores de comparación*/
+/*
+<
+
+>
+
+>=
+
+<=
+
+==
+
+&& "AND"
+
+|| "OR"
+
+!= "NOT"
+
+*/
+
+
+
+let nombreEjemplo = "Viviana";
+let edad = 10;
+let edadPersona2 = 35;
+let mayorEdad = true;
+let estatura = 1.57;
+
+/*CONDICIONALES*/
+ 
+/*if
+if else
+switch*/
+
+
+/*if(edad >18){
+    console.log("Es mayor de edad")
+}*/
+
+/*if(edad >18){
+    console.log("Es mayor de edad")
+    switch(edad){
+        case 20: 
+        console.log("Tiene 20 años")
+        break;
+        case 30:
+        console.log("Tiene 30 años")
+        break; 
+    }
+} else{
+    console.log("Es menor de edad")
+}*/
+
+/*if(edad==10 && edadPersona2==35){
+    console.log("si funcionó la comparación")
+}*/
+
+/*if(edad==10 || edadPersona2==35){
+    console.log("si funcionó la comparación or")
+}*/
+
+if(edad!=10){
+    console.log("si funcionó la comparación Not")
+} else{
+    console.log("si es igual a 10")
+}
+
